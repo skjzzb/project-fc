48b79bc25f77c08e4733b3552153af9554d29099
Saurabh Jadhav <saurabhjk07@gmail.com>
2025-06-15 14:05:15 +0000

    commited

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 
 import './App.css';
-import { useState,useRef, useEffect } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 
 function App() {
-   const divRef = useRef<HTMLDivElement>(null);
- 
+  const divRef = useRef<HTMLDivElement>(null);
+
   const [wins, setWins] = useState<number>(() => {
     const saved = localStorage.getItem('fut-wins');
     return saved ? parseInt(saved) : 0;
@@ -24,25 +24,25 @@ function App() {
 
   return (
     <div className="app">
-        
-        
-        <div ref={divRef} className="scoreboard-ui">
 
-            <div className="fut-badge-overlay">
-            {/* <img src="/ut-badge.png" alt="FUT Badge" /> */}
-             {/* <img src={utBadge} alt="UT Badge" /> */}
-          </div>
-   <div className="trapezoid">
-              <div className="fut-left-glow"></div>
+
+      <div ref={divRef} className="scoreboard-ui">
+
+        <div className="fut-badge-overlay">
+          {/* <img src="/ut-badge.png" alt="FUT Badge" /> */}
+          {/* <img src={utBadge} alt="UT Badge" /> */}
+        </div>
+        <div className="trapezoid">
+          <div className="fut-left-glow"></div>
           <div className="wins-section">
             {/* <button className="btn" onClick={() => setWins(wins + 1)}>+</button> */}
             <span className="value" onClick={() => setWins(Math.max(0, wins - 1))}>{wins}</span>
             <span className="label" onClick={() => setWins(wins + 1)}>WINS</span>
-            
+
             {/* <button className="btn" onClick={() => setWins(Math.max(0, wins - 1))}>-</button> */}
           </div>
 
-          
+
 
           <div className="losses-section">
             {/* <button className="btn" onClick={() => setLosses(losses + 1)}>+</button> */}
@@ -50,12 +50,12 @@ function App() {
             <span className="value" onClick={() => setLosses(Math.max(0, losses - 1))}>{losses}</span>
             {/* <button className="btn" onClick={() => setLosses(Math.max(0, losses - 1))}>-</button> */}
           </div>
-            <div className="fut-right-glow"></div>
-            {/* <div className="fut-badge-overlay"> */}
-            {/* <img src="/ut-badge.png" alt="FUT Badge" /> */}
-             {/* <img src={utBadge} alt="UT Badge" /> */}
+          <div className="fut-right-glow"></div>
+          {/* <div className="fut-badge-overlay"> */}
+          {/* <img src="/ut-badge.png" alt="FUT Badge" /> */}
+          {/* <img src={utBadge} alt="UT Badge" /> */}
           {/* </div> */}
-         
+
 
         </div>
       </div>
